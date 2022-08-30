6f95c664f0ffd19c99eaabf3172b6af3bb334e3f
votuan8425 <sgkcd1231@gmail.com>
2022-08-30 07:22:14 +0700

    update project

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/img/CV.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Dowload CV</a>
+        <a href={CV} download="CV-MinhTuanVo-ReactJS.pdf" className='btn'>Dowload CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
