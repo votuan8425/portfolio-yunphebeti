4ab47bf9e889e3b418d672234bd6b3fdba0e51ca
votuan8425 <sgkcd1231@gmail.com>
2022-09-07 21:43:38 +0700

    commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './portfolio.scss'
 import PJ1 from '../../assets/img/image-pj1.jpg'
+import PJ2 from '../../assets/img/image-pj2.jpg'
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -15,6 +17,16 @@ const Portfolio = () => {
           <div className="portfolio__item-cta">
             <a href="https://github.com/votuan8425/Yun-NFTs-ReactJS" rel="noreferrer" className='btn' target='_blank'>Github</a>
             <a href="https://yunphe-nft.netlify.app" target='_blank' rel="noreferrer" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={PJ1} alt="" />
+          </div>
+          <h3>NFT Store</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/votuan8425/NextJS-Ecomerce" rel="noreferrer" className='btn' target='_blank'>Github</a>
+            <a href="https://next-ecomerce-yunphe.vercel.app" target='_blank' rel="noreferrer" className='btn btn-primary'>Live Demo</a>
           </div>
         </article>
       </div>
