f0ffd5992df535cd9d03ea32996d437bf97ffca8
votuan8425 <sgkcd1231@gmail.com>
2022-09-19 11:33:23 +0700

    updated 19.9

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './portfolio.scss'
 import PJ1 from '../../assets/img/image-pj1.jpg'
 import PJ2 from '../../assets/img/image-pj2.png'
+import PJ3 from '../../assets/img/image-pj3.png'
 
 const Portfolio = () => {
   return (
@@ -27,6 +28,16 @@ const Portfolio = () => {
           <div className="portfolio__item-cta">
             <a href="https://github.com/votuan8425/NextJS-Ecomerce" rel="noreferrer" className='btn' target='_blank'>Github</a>
             <a href="https://next-ecomerce-yunphe.vercel.app" target='_blank' rel="noreferrer" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={PJ3} alt="" />
+          </div>
+          <h3>Giphy App</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/votuan8425/Giphy-React-App" rel="noreferrer" className='btn' target='_blank'>Github</a>
+            <a href="https://yunphe-giphy-react-app.vercel.app" target='_blank' rel="noreferrer" className='btn btn-primary'>Live Demo</a>
           </div>
         </article>
       </div>
